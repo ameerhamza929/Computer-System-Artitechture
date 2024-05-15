ecda1c4ab6c621eb630137d3a5378f97b93cd870
ameerhamza929 <153546668+ameerhamza929@users.noreply.github.com>
2024-05-15 11:12:25 +0500

    Update memory.v

--- memory.v
+++ memory.v
@@ -1,6 +1,9 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
 module memory
 (	input [31:0]pc_address,
-	input [31:0]alu_address, 
+	input [4:0]alu_address, 
 	input [31:0]data_in, 
 	output reg [31:0] data_out,  
 	input clk,
@@ -14,9 +17,11 @@ module memory
 
 	
 	initial begin
-		mem_memory [0] = 32'b000000_00001_00010_00011_000000_00000; //add $1, $2, $3
-		mem_memory [1] = 32'b000000_00001_00010_00011_000000_00001;
-		mem_memory [2] = 32'b000000_00001_00010_00011_000000_10000;
+		mem_memory [0] = 32'b000000_00001_00010_00011_000000_00010; //add $1, $2, $3
+		mem_memory [1] = 32'b001000_00000_00000_00000_000000_00011;
+		mem_memory [2] = 32'b000000_00001_00010_00011_000000_00010;
+		mem_memory [3] = 32'b000000_00001_00010_00011_000000_10000;
+		
 	end
 	
 	
@@ -26,17 +31,17 @@ module memory
 				mem_memory[alu_address]<=data_in;			
 			end
 	end
-	always @(posedge clk or posedge rst)begin
+	always @(*)begin
 		if(rst)begin
-			data_out<=0;
+			data_out=0;
 		end
 		else begin
 			if(memread)begin
 				if(!IorD)begin
-					data_out<=mem_memory [pc_address] ; 
+					data_out=mem_memory [pc_address] ; 
 				end	
 				else
-					data_out<=mem_memory [alu_address] ;
+					data_out=mem_memory [alu_address] ;
 			end	
 		end		
 	end
